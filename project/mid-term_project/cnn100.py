4c5278d3083c9de8547e9a77674f6aabfb25ec83
henry <773542531@qq.com>
2023-05-04 17:02:23 +0800

    cnn100 performance

--- project/mid-term_project/cnn100.py
+++ project/mid-term_project/cnn100.py
@@ -6,6 +6,12 @@ import torchvision.transforms as transforms
 from torchvision.datasets import ImageFolder
 from torch.utils.data import DataLoader
 
+# import os
+# os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+# devicee = torch.device("cuda")
+# devicee = torch.device("cpu")
+devicee = torch.device("cuda:0")
+
 # 定义卷积神经网络模型
 class Net(nn.Module):
     def __init__(self):
@@ -45,16 +51,18 @@ test_dataset = ImageFolder('./face_classification_100/test_sample', transform=tr
 test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
 
 # 定义损失函数和优化器
-net = Net()
-criterion = nn.CrossEntropyLoss()
+net = Net().to(devicee)
+criterion = nn.CrossEntropyLoss().to(devicee)
 optimizer = optim.Adam(net.parameters(), lr=0.001)
 
 # 训练模型
-for epoch in range(10):  # 多次循环数据集
+for epoch in range(60):  # 多次循环数据集
     running_loss = 0.0
     for i, data in enumerate(train_loader, 0):
         # 获取输入数据
         inputs, labels = data
+        inputs = inputs.to(devicee)
+        labels = labels.to(devicee)
         # print(inputs.shape)
         # print(labels.shape)
 
@@ -71,9 +79,9 @@ for epoch in range(10):  # 多次循环数据集
 
         # 输出统计信息
         running_loss += loss.item()
-        if i % 100 == 99:
-            # 每100个小批次打印一次统计信息
-            print('[%d, %5d] loss: %.3f' %
+        if i % 10 == 9:
+            # 每10个小批次打印一次统计信息
+            print('[%d, %5d] loss: %.5f' %
                 (epoch + 1, i + 1, running_loss / 100))
             running_loss = 0.0
 
@@ -82,6 +90,8 @@ total = 0
 with torch.no_grad():
     for data in dev_loader:
         inputs, labels = data
+        inputs = inputs.to(devicee)
+        labels = labels.to(devicee)
         outputs = net(inputs)
         _, predicted = torch.max(outputs.data, 1)
         total += labels.size(0)
@@ -93,6 +103,8 @@ total = 0
 with torch.no_grad():
     for data in test_loader:
         inputs, labels = data
+        inputs = inputs.to(devicee)
+        labels = labels.to(devicee)
         outputs = net(inputs)
         _, predicted = torch.max(outputs.data, 1)
         total += labels.size(0)
