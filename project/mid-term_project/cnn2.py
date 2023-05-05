319ae03a72da327326adccb501fae9c40b1599ac
henry <773542531@qq.com>
2023-05-05 21:41:59 +0800

    新架构

--- project/mid-term_project/cnn2.py
+++ project/mid-term_project/cnn2.py
@@ -1,117 +1,57 @@
-import cv2
 import torch
-import numpy as np
 import torch.nn as nn
 import torch.optim as optim
-import torch.nn.functional as F
-import torchvision.transforms as transforms
-from torchvision.datasets import ImageFolder
-from torch.utils.data import DataLoader
 
-
-# 定义卷积神经网络模型
+# 定义网络结构
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, padding=1)
-        self.bn1 = nn.BatchNorm2d(16)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
-        self.bn2 = nn.BatchNorm2d(32)
-        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
-        self.bn3 = nn.BatchNorm2d(64)
-        self.conv4 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
-        self.bn4 = nn.BatchNorm2d(128)
-        self.fc1 = nn.Linear(128 * 8 * 8, 512)
-        self.fc2 = nn.Linear(512, 256)
-        self.fc3 = nn.Linear(256, 2)
-
+        self.conv1 = nn.Conv2d(3, 32, 3)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(32, 64, 3)
+        self.conv3 = nn.Conv2d(64, 128, 3)
+        self.fc1 = nn.Linear(128 * 14 * 14, 512)
+        self.fc2 = nn.Linear(512, 500)
+        self.softmax = nn.Softmax(dim=1)
+        
     def forward(self, x):
-        x = self.bn1(F.relu(self.conv1(x)))
-        x = F.max_pool2d(self.bn2(F.relu(self.conv2(x))), 2)
-        x = self.bn3(F.relu(self.conv3(x)))
-        x = F.max_pool2d(self.bn4(F.relu(self.conv4(x))), 2)
-        x = x.view(-1, 128 * 8 * 8)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = self.pool(nn.functional.relu(self.conv1(x)))
+        x = self.pool(nn.functional.relu(self.conv2(x)))
+        x = self.pool(nn.functional.relu(self.conv3(x)))
+        x = x.view(-1, 128 * 14 * 14)
+        x = nn.functional.relu(self.fc1(x))
+        x = self.softmax(self.fc2(x))
         return x
 
-# 加载数据集
-transform = transforms.Compose([transforms.Resize((64, 64)),
-                                transforms.ToTensor()])
-train_dataset = ImageFolder('path/to/training/dataset', transform=transform)
-train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-test_dataset = ImageFolder('path/to/testing/dataset', transform=transform)
-test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+# 实例化网络
+net = Net()
 
 # 定义损失函数和优化器
-net = Net()
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(net.parameters(), lr=0.001)
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-# 训练模型
-for epoch in range(10):  # 多次循环数据集
+
+# 训练网络
+for epoch in range(num_epochs):
     running_loss = 0.0
-    for i, data in enumerate(train_loader, 0):
-        # 获取输入数据
+    for i, data in enumerate(trainloader, 0):
         inputs, labels = data
-
-        # 梯度清零
         optimizer.zero_grad()
-
-        # 正向传递
         outputs = net(inputs)
         loss = criterion(outputs, labels)
-
-        # 反向传递和优化
         loss.backward()
         optimizer.step()
-
-        # 输出统计信息
         running_loss += loss.item()
-        if i % 100 == 99:
-            # 每100个小批次打印一次统计信息
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 100))
-
-            running_loss = 0.0
-
-# 测试模型
+    print('Epoch %d, loss: %.3f' % (epoch + 1, running_loss / len(trainloader)))
+    
+# 测试网络
 correct = 0
 total = 0
 with torch.no_grad():
-    for data in test_loader:
-        inputs, labels = data
-        outputs = net(inputs)
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
         _, predicted = torch.max(outputs.data, 1)
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
-
-print('Accuracy of the network on the test images: %d %%' % (
-    100 * correct / total))
-
-# 使用模型进行实时人脸识别
-net.eval()  # 切换到评估模式
-face_cascade = cv2.CascadeClassifier('path/to/haarcascade_frontalface_default.xml')
-cap = cv2.VideoCapture(0)
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    for (x, y, w, h) in faces:
-        face_img = gray[y:y+h, x:x+w]
-        face_img = cv2.resize(face_img, (64, 64))
-        face_tensor = transform(face_img).unsqueeze(0)
-        outputs = net(face_tensor)
-        _, predicted = torch.max(outputs.data, 1)
-        label = train_dataset.classes[predicted.item()]
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        cv2.putText(frame, label, (x, y-10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
-    cv2.imshow('Face Recognition', frame)
-    if cv2.waitKey(1) == ord('q'):
-        break
-cap.release()
-cv2.destroyAllWindows()
+print('Accuracy: %.2f %%' % (100 * correct / total))
