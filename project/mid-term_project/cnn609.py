bfd4de855db0ade3a3172badd302f4d68e59fe0d
henry <773542531@qq.com>
2023-05-13 15:23:39 +0800

    a lot

--- project/mid-term_project/cnn609.py
+++ project/mid-term_project/cnn609.py
@@ -6,6 +6,7 @@ import torchvision.transforms as transforms
 from torchvision.datasets import ImageFolder
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
+
 # from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 devicee = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -19,7 +20,7 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.conv1 = nn.Sequential(
             nn.BatchNorm2d(3),
-            nn.Conv2d(3, 96, 11, 4, 1,),
+            nn.Conv2d(3, 96, 7, 2, 1,),
             nn.ReLU(),
             nn.MaxPool2d(2),
             nn.BatchNorm2d(96),
@@ -28,7 +29,10 @@ class Net(nn.Module):
             nn.Conv2d(96, 256, 3, 1, 1), nn.ReLU(), nn.MaxPool2d(2), nn.BatchNorm2d(256)
         )
         self.conv3 = nn.Sequential(
-            nn.Conv2d(256, 384, 3, 1, 1), nn.ReLU(), nn.MaxPool2d(2), nn.BatchNorm2d(384)
+            nn.Conv2d(256, 384, 3, 1, 1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.BatchNorm2d(384),
         )
         self.conv4 = nn.Sequential(
             nn.Conv2d(384, 512, 3, 1, 1),
@@ -45,8 +49,9 @@ class Net(nn.Module):
         # self.conv9 = nn.Sequential(
         #     nn.Conv2d(16384, 32768, 3, 1, 1), nn.ReLU(), nn.MaxPool2d(2),nn.BatchNorm2d(32768)
         # )
-        self.fc1 = nn.Linear(512*9, 2048)
-        self.fc2 = nn.Linear(2048, 500)
+        self.fc1 = nn.Linear(512 * 6*6, 2048)
+        self.fc2 = nn.Linear(2048, 2048)
+        self.fc3 = nn.Linear(2048, 500)
         self.dropout = nn.Dropout(p=0.5)
 
     def forward(self, x):
@@ -63,6 +68,7 @@ class Net(nn.Module):
         x = x.view(x.size(0), -1)  # 展平多维的卷积图层
         x = self.fc1(x)
         x = self.fc2(x)
+        x = self.fc3(x)
         x = self.dropout(x)
         return x
 
@@ -91,8 +97,18 @@ test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
 # 定义损失函数和优化器
 net = Net().to(devicee)
 criterion = nn.CrossEntropyLoss().to(devicee)
-optimizer = optim.SGD(net.parameters(), lr=0.009)
-scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, step_size=4, gamma=0.9)
+optimizer = optim.SGD(net.parameters(), lr=0.007)
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+    optimizer,
+    factor=0.7,
+    patience=10,
+    verbose=False,
+    threshold=0.0001,
+    threshold_mode="rel",
+    cooldown=0,
+    min_lr=0,
+    eps=1e-08,
+)
 train_loss = []
 dev_acc = []
 test_acc = []
@@ -126,10 +142,10 @@ for epoch in range(100):  # 多次循环数据集
         running_loss += loss.item()
         if i % 10 == 9:
             # 每10个小批次打印一次统计信息
+            scheduler.step(running_loss / 10)
             print("[%d, %5d] loss: %.5f" % (epoch + 1, i + 1, running_loss / 10))
             running_loss = 0.0
-    scheduler.step()
-    print("lr = ", optimizer.state_dict()['param_groups'][0]['lr'])
+    print("lr = ", optimizer.state_dict()["param_groups"][0]["lr"])
     net.eval()
     correct = 0
     total = 0
@@ -159,6 +175,7 @@ for epoch in range(100):  # 多次循环数据集
     print("Accuracy of the network on the test images: %d %%" % (100 * correct / total))
     test_acc.append((100 * correct / total))
 
+torch.save(net.state_dict(), 'model.pkl')
 
 plt.plot(train_loss)
 plt.title("training loss")
